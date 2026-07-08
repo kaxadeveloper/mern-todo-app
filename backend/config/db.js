d0eb003da696aae8d3a99713595b4b5317e3e983
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-07-08 15:02:15 +0400

    import icons

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+dns.setDefaultResultOrder("ipv4first");
 
 export const connectDB = async () => {
     try {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
+import { MdOutlineDone } from "react-icons/md";
+import { IoClose } from "react-icons/ioS";
+import { MdModeEditOutline } from "react-icons/md";
+import { FaTrash } from "react-icons/fa6";
+import { IoClipboardOutline } from "react-icons/io5";
+import axios from "axios";
 
 function App() {
   const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = async (e) => {
+    e.preventDefault();
+    if (!newTodo.trim()) return;
+    try {
+      const response = await axios.post("/api/todos", { text: newTodo })
+      setTodos([...todos, response.data])
+      setNewTodo("");
+    } catch (error) {
+      console.log("Error adding todo:", error);
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gradient-to-br from gray-50 to-gray-100 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-xl w-full max-w-lg p-8">
         <h1 className="text-4xl font-bold text-gray-800 mb-8">Task Manager</h1>
 
-        <form className="flex items-center gap-2 shadow-sm border border-gray-200 p-2 rounded-lg">
+        <form
+          onSubmit={addTodo}
+          className="flex items-center gap-2 shadow-sm border border-gray-200 p-2 rounded-lg">
           <input
             className="flex-1 outline-none px-3 py-2 text-gray-700 placeholder-gray-400"
             type="text"
@@ -16,8 +38,19 @@ function App() {
             placeholder="What needs to be done?"
             required
           />
-          <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium cursor-pointer">Add Task</button>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium cursor-pointer">Add Task</button>
         </form>
+        <div>
+          {todos.length === 0 ? (
+            <div></div>
+          ) : (
+            <div>
+              {todos.map((todo) => (
+                <div key={todo._id}>{todo.text}</div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
