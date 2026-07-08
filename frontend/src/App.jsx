baf71f19d1bca6744efce21534a49db65803b9cf
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-07-08 12:05:47 +0400

    start React App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from gray-50 to-gray-500">
+      <h1>Task Manager</h1>
+    </div>
+  )
+}
+
+export default App
