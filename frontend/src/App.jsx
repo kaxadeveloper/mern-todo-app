2c8c21a4bfd8d6ded71019374b9ef7c2821e26c0
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-07-09 13:52:17 +0400

    add styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,12 +41,14 @@ function App() {
   const startEditing = (todo) => {
     setEditingTodo(todo._id)
     setEditedText(todo.text)
-  }
+  };
+
+  
 
   return (
     <div className="min-h-screen bg-gradient-to-br from gray-50 to-gray-100 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-xl w-full max-w-lg p-8">
-        <h1 className="text-4xl font-bold text-gray-800 mb-8">Task Manager</h1>
+        <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">Task Manager</h1>
 
         <form
           onSubmit={addTodo}
@@ -61,11 +63,11 @@ function App() {
           />
           <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium cursor-pointer">Add Task</button>
         </form>
-        <div>
+        <div className="mt-4">
           {todos.length === 0 ? (
             <div></div>
           ) : (
-            <div>
+            <div className="flex flex-col gap-4">
               {todos.map((todo) => (
                 <div key={todo._id}>
                   {editingTodo === todo._id ? (
@@ -87,7 +89,14 @@ function App() {
                   ) : (
                     <div>
                       <div className="flex items-center justify-between">
-                        <span>{todo.text}</span>
+                        <div className="flex items-center gap-x-4">
+                          <button className={`h-6 w-6 border rounded-full flex items-center justify-center ${todo.completed ? "bg-green-500 border-green-500" : "border-gray-300 hover:border-blue-400"}`}>
+                            {!todo.completed && <MdOutlineDone />}
+                          </button>
+                          <span className="text-gray-800 font-medium">
+                            {todo.text}
+                          </span>
+                        </div>
                         <div className="flex gap-x-2">
                           <button className="p-2 text-blue-500 hover:text-blue-700 rounded-lg hover:bg-blue-50 duration-200" onClick={() => startEditing(todo)}>
                             <MdModeEditOutline />
