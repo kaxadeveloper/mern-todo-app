ef55b557184140f4fae703b12a51f1f2f9ba6943
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-07-12 07:11:31 +0400

    fixed version

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
 import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
-import cors from "cors";
-import path from "path";
-const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 app.use("/api/todos", todoRoutes);
 
-// if(process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//     });
-// }
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
@@ -29,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log(`Server started at http://localhost:${PORT}`);
 });
